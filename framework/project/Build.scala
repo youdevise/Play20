53ab3bc24c539734d3e85bb9ad7d3a8474ca6854
Marc Siegel <marc.siegel@timgroup.com>
2013-05-15 10:41:06 -0400

    Add back the jsr305 jar (required at compilation time)
    Conflicts:
    	framework/project/Build.scala

--- framework/project/Build.scala
+++ framework/project/Build.scala
@@ -192,7 +192,12 @@ object PlayBuild extends Build {
             "com.github.scala-incubator.io"     %%   "scala-io-file"            %   "0.4.0",
             "com.typesafe.akka"                 %    "akka-actor"               %   "2.0.2",
             "com.typesafe.akka"                 %    "akka-slf4j"               %   "2.0.2",
-            "com.google.guava"                  %    "guava"                    %   "10.0.1",
+            ("com.google.guava"                 %    "guava"                    %   "10.0.1" notTransitive())
+              .exclude("com.google.code.findbugs", "jsr305")
+            ,
+
+            "com.google.code.findbugs"          %    "jsr305"                   %   "2.0.0",
+
             ("org.avaje"                        %    "ebean"                    %   "2.7.3" notTransitive())
               .exclude("javax.persistence", "persistence-api")
             ,
