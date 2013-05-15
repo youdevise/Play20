e03ffb358b9566a46280afad158c8ede8733150e
Marc Siegel <marc.siegel@timgroup.com>
2013-05-15 09:41:59 -0400

    Marc/Ash: bump fluentlenium to 0.8.0
    Conflicts:
    	framework/project/Build.scala

--- framework/project/Build.scala
+++ framework/project/Build.scala
@@ -252,7 +252,7 @@ object PlayBuild extends Build {
             "org.mockito"                       %    "mockito-all"              %   "1.9.0"    %  "test",
             "com.novocode"                      %    "junit-interface"          %   "0.8"        %  "test",
             
-            "org.fluentlenium"     %    "fluentlenium-festassert"             %   "0.6.0"      %  "test"
+            "org.fluentlenium"                  %    "fluentlenium-festassert"  %   "0.8.0"      %  "test"
         )
 
         val sbtDependencies = Seq(
@@ -297,7 +297,7 @@ object PlayBuild extends Build {
             "org.specs2"                        %%   "specs2"                   %   "1.9",
             "com.novocode"                      %    "junit-interface"          %   "0.8",
             
-            "org.fluentlenium"     %    "fluentlenium-festassert"             %   "0.6.0"
+            "org.fluentlenium"                  %    "fluentlenium-festassert"  %   "0.8.0"
         )
 
     }
